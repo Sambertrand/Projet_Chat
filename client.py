bc309beb2e7cb64abd4fd944161b4aa7d2d85597
Martin Degeldt <15076@student.ecam.be>
2017-03-30 18:25:02 +0200

    Fix some buggs

--- client.py
+++ client.py
@@ -6,7 +6,7 @@
 import socket
 import sys
 import threading
-import json
+import ast
 
 
 class Chat:
@@ -14,6 +14,9 @@ class Chat:
         self.__pseudo = pseudo
         self.__avlbl = None
         self.__update = False
+        self.__running = True
+        self.__address = None
+        self.__servaddress = None
         s = socket.socket(type=socket.SOCK_DGRAM)
         s.settimeout(0.5)
         s.bind((host, port))
@@ -29,9 +32,6 @@ class Chat:
             '/clients': self._clients,
             '/connect': self._connect
         }
-        self.__running = True
-        self.__address = None
-        self.__servadress = None
         threading.Thread(target=self._receive).start()
         while self.__running:
             line = sys.stdin.readline().rstrip() + ' '
@@ -40,30 +40,30 @@ class Chat:
             param = line[line.index(' ') + 1:].rstrip()
             # Call the command handler
             if command in handlers:
-                try:
+                #try:
                     handlers[command]() if param == '' else handlers[command](param)
-                except:
-                    print("Erreur lors de l'execution de la commande.")
+                #except:
+                    #print("Erreur lors de l'execution de la commande.")
             else:
                 print('Command inconnue:', command)
 
     def _exit(self):
         print('Cy@')
         self.__running = False
-        self.__servadress = None
+        self.__servaddress = None
         self.__address = None
         self.__s.close()
 
     def _quit(self):
-        if self.__servadress is not None:
+        if self.__servaddress is not None:
             try:
                 message = b'/quit ' + self.__pseudo.encode()
                 totalsent = 0
                 while totalsent < len(message):
-                    sent = self.__s.sendto(message[totalsent:], self.__servadress)
+                    sent = self.__s.sendto(message[totalsent:], self.__servaddress)
                     totalsent += sent
                 print('Deconnecte du serveur {}:{}'.format(*self.__address))
-                self.__servadress = None
+                self.__servaddress = None
             except OSError:
                 print('Erreur lors de deconnection au serveur.')
 
@@ -83,7 +83,7 @@ class Chat:
         elif len(tokens) == 1:
             pseudo = tokens[0]
             if pseudo in self.__avlbl:
-                self.__adress = (socket.gethostbyaddr(self.__avlbl[pseudo][0])[0], int(self.__avlbl[pseudo][1]))
+                self.__address = (socket.gethostbyaddr(self.__avlbl[pseudo][0])[0], int(self.__avlbl[pseudo][1]))
                 print('Connecte a {} sur {}:{}'.format(pseudo, *self.__address))
 
     def _send(self, param):
@@ -101,28 +101,31 @@ class Chat:
 
     def _receive(self):
         while self.__running:
-            try:
-                data, address = self.__s.recvfrom(1024)
-                print(data.decode())
-            except socket.timeout:
-                pass
-            except OSError:
-                return
+            if self.__address is not None:
+                try:
+                    data, address = self.__s.recvfrom(1024)
+                    print(data.decode())
+                except socket.timeout:
+                    pass
+                except OSError:
+                    return
 
     def _clients(self):
-        if self.__servadress is not None:
+        if self.__servaddress is not None:
             try:
                 message = b'/clients ' + self.__pseudo.encode()
                 totalsent = 0
                 while totalsent < len(message):
-                    sent = self.__s.sendto(message[totalsent:], self.__servadress)
+                    sent = self.__s.sendto(message[totalsent:], self.__servaddress)
                     totalsent += sent
             except OSError:
                 print('Erreur lors de la reception du message.')
             while self.__update is False:
                 try:
                     data, address = self.__s.recvfrom(1024)
-                    self.__avlbl = json.loads(data.decode())
+                    print(data.decode())
+                    print(type(data.decode()))
+                    self.__avlbl = ast.literal_eval(data.decode())
                     print(self.__avlbl)
                     self.__update = True
                 except socket.timeout:
@@ -135,19 +138,19 @@ class Chat:
             print('Connectez vous a un serveur')
 
     def _connect(self, param):
+        self._quit()
         data = b'0'
         # data is 0 if the nickname is already taken. 1 if not.
         while data == b'0':
-            self._quit()
             tokens = param.split(' ')
             if len(tokens) == 2:
                 try:
-                    self.__servadress = (socket.gethostbyaddr(tokens[0])[0], int(tokens[1]))
+                    self.__servaddress = (socket.gethostbyaddr(tokens[0])[0], int(tokens[1]))
                     # envoye le pseudo au serveur
                     message = b'/connect ' + self.__pseudo.encode()
                     totalsent = 0
                     while totalsent < len(message):
-                        sent = self.__s.sendto(message[totalsent:], self.__servadress)
+                        sent = self.__s.sendto(message[totalsent:], self.__servaddress)
                         totalsent += sent
                     while self.__update is False:
                         try:
@@ -157,10 +160,15 @@ class Chat:
                             self.__update = True
                         except socket.timeout:
                             pass
+                    print('Connecte au serveur {}:{}'.format(*self.__servaddress))
                     self.__update = False
+
                 except OSError:
                     print("Erreur lors de la connection au serveur.")
-        print('Connecte au serveur {}:{}'.format(*self.__servadress))
+            else:
+                print('veuillez entrer une adresse valide')
+                break
+
 
     def _help(self):
         pass
